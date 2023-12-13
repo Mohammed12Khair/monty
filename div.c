514743b6262a9bc35cd321f42a9dc642b7f9cd79
Ahmed Basher <basherahmed362@gmial.com>
2023-12-13 16:27:52 +0400

    u

--- div.c
+++ div.c
@@ -9,10 +9,10 @@ void div_nodes(stack_t **stack, unsigned int line_number)
 	int sum;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_number, "div");
+		ERROR(8, line_number, "div");
 
 	if ((*stack)->n == 0)
-		more_err(9, line_number);
+		ERROR(9, line_number);
 	(*stack) = (*stack)->next;
 	sum = (*stack)->n / (*stack)->prev->n;
 	(*stack)->n = sum;
